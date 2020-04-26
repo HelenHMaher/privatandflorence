6a49777be55472c4b4acca635b3adfc2a32f53d6
HelenHMaher <helenharlanmaher@gmail.com>
2020-04-26 11:34:55 +0200

    Updates

---
+++ precache-manifest.cf73b990e04058fb835ffa4c39c17f6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37136b892778fd72471a6869cbb55f97",
+    "url": "/privateandflorence/index.html"
+  },
+  {
+    "revision": "7998d793e88e1dc32513",
+    "url": "/privateandflorence/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "055ca16c9123a4ff42fb",
+    "url": "/privateandflorence/static/js/2.cde3ff6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/privateandflorence/static/js/2.cde3ff6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7998d793e88e1dc32513",
+    "url": "/privateandflorence/static/js/main.6f3b4e10.chunk.js"
+  },
+  {
+    "revision": "64340868baf90f7cde1a",
+    "url": "/privateandflorence/static/js/runtime-main.329b254a.js"
+  }
+]);
