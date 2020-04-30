7b32e07220e7a8e021597c9d02d2953413a88f85
HelenHMaher <helenharlanmaher@gmail.com>
2020-04-30 18:01:38 +0200

    background image

--- src/welcome/index.js
+++ src/welcome/index.js
@@ -3,22 +3,20 @@ import React from "react";
 export const Welcome = () => {
   return (
     <section id="welcome" className="welcome">
-      <div className="grid">
-        <div className="main-image">
-          <figure id="main-image">
-            <img
-              id="engagement-photo"
-              src={require("../photos/engagementPhoto.jpg")}
-              alt="Florence and Private on their engagement"
-            />
-          </figure>
-        </div>
-
-        <div className="title">
-          <h1>Privat and Florence</h1>
-          <p>August 21, 2021</p>
-        </div>
+      <div className="main-image">
+        <figure id="main-image">
+          <img
+            id="engagement-photo"
+            src={require("../photos/engagementPhoto.jpg")}
+            alt="Florence and Private on their engagement"
+          />
+        </figure>
       </div>
+      <div className="title">
+        <h1>Privat and Florence</h1>
+        <p>August 21, 2021</p>
+      </div>
+
       <div className="email">
         <form id="form" action="__">
           <div className="email-feild">
