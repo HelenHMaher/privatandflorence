64cc41bd8a209837c8d3efa258dbd22dc98315e5
HelenHMaher <helenharlanmaher@gmail.com>
2020-05-07 18:44:17 +0200

    working out links

--- src/topics/event-details/index.js
+++ src/topics/event-details/index.js
@@ -6,37 +6,44 @@ export const EventDetails = () => {
       <h2 className="header">Event Details</h2>
       <ul id="detailsnav">
         <li>
-          <a href="#hillsboro">Travel and Accomodations</a>
-        </li>
-        <li>
           <a href="#ceremoney">The Ceremoney</a>
         </li>
         <li>
           <a href="#reception">The Reception</a>
         </li>
+        <li>
+          <a href="#hillsboro">Travel and Accomodations</a>
+        </li>
       </ul>
-      <h3 id="hillsboro">Travel and Accomodations</h3>
       <h3 id="ceremoney">The Ceremoney</h3>
       <p>
         The ceremoney will take place at{" "}
         <a
           id="stMatthewsLink"
-          href="https://www.stmatthewhillsboro.org/"
+          href="https://www.google.com/maps/place/St.+Matthew+Catholic+Church/@45.5185101,-122.9886606,17z/data=!3m1!4b1!4m5!3m4!1s0x54951aa3c9941b4f:0xbb7f68a76f2d7e86!8m2!3d45.5185101!4d-122.9864666"
           target="_blank"
         >
           St. Matthew's Church
         </a>
-        . 475 SE 3rd Ave, Hillsboro, Oregon 97123 (also on{" "}
+        . 475 SE 3rd Ave, Hillsboro, Oregon 97123 Google Maps{" "}
         <a
-          id="stMatthewsFacebook"
+          id="churchWebsite"
+          href="https://www.stmatthewhillsboro.org/"
           target="_blank"
-          href="https://www.facebook.com/stmatthewhillsboro/"
         >
-          Facebook
+          St. Matthew's Catholic Church Website
         </a>
-        )
       </p>
       <h3 id="reception">The Reception</h3>
+      The reception will take place at{" "}
+      <a
+        id="peterkortroses"
+        href="https://www.google.com/maps/place/Peterkort+Roses/@45.4343164,-122.9718975,17z/data=!3m1!4b1!4m5!3m4!1s0x549511584f6dc597:0x39f32921471f1123!8m2!3d45.4343164!4d-122.9697035"
+        target="_blank"
+      >
+        Peterkort Roses.
+      </a>
+      <h3 id="hillsboro">Travel and Accomodations</h3>
     </section>
   );
 };
